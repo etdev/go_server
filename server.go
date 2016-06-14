781c66c0a355411a856e4b52e4cdd27210ceba11
Eric Turner <ericturnerdev@gmail.com>
2016-06-14 14:57:42 +0900

    Fix root directory

--- server.go
+++ server.go
@@ -7,5 +7,8 @@ import (
 
 func main() {
 	var port = flag.String("p", "8000", "Please enter a valid port")
-	http.ListenAndServe(":"+*port, http.FileServer(http.Dir("public")))
+	var root_dir = flag.String("r", "public", "Please enter a valid root directory")
+	flag.Parse()
+
+	http.ListenAndServe(":"+*port, http.FileServer(http.Dir(*root_dir)))
 }
